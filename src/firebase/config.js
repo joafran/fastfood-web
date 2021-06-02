d550750cbdb4ac1738b90ce5824853271f3a4032
joafran <joafran0016@gmail.com>
2021-06-02 11:03:06 -0300

    added .env config and fixed logo error

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,14 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBAmnxOFXSNB2-CRvV5nwj30TflESqC8Ms",
-    authDomain: "fast-food-69800.firebaseapp.com",
-    projectId: "fast-food-69800",
-    storageBucket: "fast-food-69800.appspot.com",
-    messagingSenderId: "703200669110",
-    appId: "1:703200669110:web:eac16173434fa949116517",
-    measurementId: "G-XEHKZVQ4XJ"
-  };
+import dotenv from 'dotenv';
+dotenv.config()
 
-export default firebaseConfig;
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+  export default firebaseConfig;
